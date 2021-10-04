a78feb27ee13204c66271c9606ec0f0e9458c7f9
Tomas Molina <jtml.mass@gmail.com>
2021-10-04 18:59:49 -0400

    Set springboot up with Web, Rest, lombock, and JWT

---
+++ src/main/java/com/isc517/mocky/MockyApplication.java
@@ -0,0 +1,13 @@
+package com.isc517.mocky;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MockyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MockyApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/isc517/mocky/MockyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.isc517.mocky;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MockyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
