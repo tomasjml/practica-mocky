4278ae7844903c83fbde7c68649a8b2eabfd568a
Tomas Molina <jtml.mass@gmail.com>
2021-10-17 16:10:58 -0400

    Add API routes configuration and .env

---
+++ frontend/src/modules/core/api/path.js
@@ -0,0 +1,31 @@
+import { appConfig } from "../../config/global";
+
+const host = appConfig.apiHost;
+
+export let serializeURL = function (obj, prefix) {
+	let str = [],
+		p;
+	for (p in obj) {
+		// eslint-disable-next-line no-prototype-builtins
+		if (obj.hasOwnProperty(p)) {
+			let k = prefix ? prefix + "[" + p + "]" : p,
+				v = obj[p];
+			str.push(
+				v !== null && typeof v === "object" ? serializeURL(v, k) : encodeURIComponent(k) + "=" + encodeURIComponent(v)
+			);
+		}
+	}
+	return str.join("&");
+};
+
+export const loginPath = {
+	login: () => `${host}/login`
+};
+
+export const userPath = {
+	createUser: () => `${host}/public/user/`,
+	updateUser: id => `${host}/user/update/${id}`,
+	changeUserPassword: id => `${host}/user/resetPassword/${id}`,
+	activateInactivateUser: id => `${host}/user/activateInactivateUser/${id}`,
+	fetchUsers: () => `${host}/user/all`
+};
